e994560bf0867aae3bb228e3476c72e9411205d1
Kimberly Nelson <129911891+gypze@users.noreply.github.com>
2024-12-23 12:06:18 -0600

    server and db setup

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
 require('dotenv').config();
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection')
+
+
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(session(sess));
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+const hbs = exphbs.create({ helpers,extname: 'hbs' });
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync().then(() => {
+    console.log('Database connected');
+}).catch(err => {
+    console.error('Unable to sync database:', err);
 });
 
 app.listen(PORT, () => {
