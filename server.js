acdb49fd89bba57abeeff9c83e22d7f463221919
Kimberly Nelson <129911891+gypze@users.noreply.github.com>
2024-12-23 11:41:31 -0600

    setup

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
